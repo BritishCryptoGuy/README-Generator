070ceafd6b881f40dff5a01f801d8cb0ab21e846
Kieran <108032076+BritishCryptoGuy@users.noreply.github.com>
2023-01-22 10:47:16 +0000

    All questions created. Code created to pass data to Markdown. Now need to fully implement

--- index.js
+++ index.js
@@ -2,12 +2,100 @@ const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+// let options = [];
 
 // array of questions for user
-const questions = ["Would you like to generate a README.md file?"];
+const questions = [
+  {
+    name: "createReadMe",
+    question: "Would you like to generate a README.md file?",
+  },
+  { name: "title", question: "What is the title of your project?" },
+  {
+    name: "descriptionText",
+    question: "What would you like your description to be?",
+  },
+  {
+    name: "installationText",
+    question: "Please input installation text section here",
+  },
+  { name: "usageText", question: "Please input usage section text here" },
+  {
+    name: "contributionText",
+    question: "Please input contribution section text here",
+  },
+  { name: "testsText", question: "Please input tests section text here" },
+  {
+    name: "licenseChoice",
+    question: "What license would you like for this project?",
+  },
+  {
+    name: "githubUsername",
+    question: "What is your github profile username?",
+  },
+  {
+    name: "emailAddress",
+    question: "What email address would you like to add for feedback?",
+  },
+  {
+    name: "fileLocation",
+    question:
+      "Input a path to where you would like to save your generated README",
+  },
+];
 
 // function to write README file
 function writeToFile(fileName, data) {}
+
+//function to ask relevant questions
+function readmeData() {
+  inquirer
+    .prompt([
+      {
+        name: questions[1].name,
+        message: questions[1].question,
+      },
+      {
+        name: questions[2].name,
+        message: questions[2].question,
+      },
+      {
+        name: questions[3].name,
+        message: questions[3].question,
+      },
+      {
+        name: questions[4].name,
+        message: questions[4].question,
+      },
+      {
+        name: questions[5].name,
+        message: questions[5].question,
+      },
+      {
+        name: questions[6].name,
+        message: questions[6].question,
+      },
+      {
+        type: "rawlist",
+        name: questions[7].name,
+        message: questions[7].question,
+        choices: ["MIT", "GPL", "Apache"],
+      },
+      {
+        name: questions[8].name,
+        message: questions[8].question,
+      },
+      {
+        name: questions[8].name,
+        message: questions[8].question,
+      },
+      {
+        name: questions[9].name,
+        message: questions[9].question,
+      },
+    ])
+    .then((data) => generateMarkdown(data));
+}
 
 // function to initialize program
 function init() {
@@ -15,16 +103,16 @@ function init() {
     .prompt([
       {
         type: "confirm",
-        name: "answer",
-        message: questions[0],
+        name: questions[0].name,
+        message: questions[0].question,
       },
     ])
     .then((ans) => {
-      if (!ans.answer) {
+      if (!ans.createReadMe) {
         console.log("Maybe next time :(");
         return;
       }
-      console.log("Great Choice!");
+      readmeData();
     });
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,32 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  console.log(
+    `# ${data.title}
+
+    ## Description
+    ${data.descriptionText}
+    
+  ## Installation
+  ${data.installationText}
+
+  ## Usage 
+  ${data.usageText}
+
+  ## License 
+  ${data.licenseChoice}
+
+  ## Contributing guidelines
+  ${data.contributionText}
+
+  ## Tests
+  ${data.testsText}
+
+  ## Questions
+  ${data.githubUsername}
+  ${data.emailAddress}
+
+  `
+  );
   return `# ${data.title}
 
 `;
